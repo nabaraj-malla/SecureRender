c6d7689d193043d76f2832833193c44f8c3eba0d
nabaraj malla <nabarajmalla4000@gmail.com>
2024-05-20 23:01:28 +0545

    initial commit

---
+++ src/middleware/auth.js
@@ -0,0 +1,13 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+
+export const auth = (req, res, next) => {
+  // Write your code here
+  if (req.session.userEmail) {
+    next();
+  } else {
+    return res.render("msgPage", {
+      message: "login first to access secure page",
+    });
+  }
+};
